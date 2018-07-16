f4a32cfe5278f616e80fbc14724228ed6fa8fd3b
James Moore <james@knowthen.com>
2018-07-16 15:55:41 -0700

    Authors Dataloader part 1

--- hackerbookreview_starter/server/src/author.js
+++ hackerbookreview_starter/server/src/author.js
@@ -1,4 +1,30 @@
+import { groupBy, map } from 'ramda';
+import DataLoader from 'dataloader';
 import query from './db';
+
+export async function findAuthorsByBookIds(ids) {
+  const sql = `
+  select 
+  hb.author.*,
+  hb.book_author.book_id
+  from hb.author inner join hb.book_author
+    on hb.author.id = hb.book_author.author_id
+  where hb.book_author.book_id = ANY($1);
+  `;
+  const params = [ids];
+  try {
+    const result = await query(sql, params);
+    const rowsById = groupBy(author => author.bookId, result.rows);
+    return map(id => rowsById[id] , ids);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export function findAuthorsByBookIdsLoader() {
+  return new DataLoader(findAuthorsByBookIds);
+}
 
 export async function authorsByBookId(id) {
   const sql = `
